9e2a7eaa7cf92584e42ddabfaff707359b2e239e
DenisSeroshtan <haron_777@mail.ru>
2018-07-08 01:29:06 +0500

    add method paint

--- ui.js
+++ ui.js
@@ -12,6 +12,37 @@ class UI {
 
   }
 
+  static getDirectionWind(deg) {
+    deg = +deg;
+    let direction;
+
+    if (deg >= 340 && deg <= 360 || deg >= 0 && deg <= 20) direction = 'с'
+    else if (deg > 20 && deg <= 70) direction = 'с-в'
+    else if (deg > 70 && deg <= 110) direction = 'в'
+    else if (deg > 110 && deg <= 160) direction = 'ю-в'
+    else if (deg > 160 && deg <= 200) direction = 'ю'
+    else if (deg > 200 && deg <= 250) direction = 'ю-з'
+    else if (deg > 250 && deg <= 290) direction = 'з'
+    else if (deg > 290 && deg <= 340) direction = 'с-з'
+    else direction = 'deg'
+
+    return direction;
+  }
+
+  static getTime(timeUTC) {
+    const date = new Date(timeUTC * 1000);
+
+    const hours = date.getHours();
+
+    const minutes = "0" + date.getMinutes();
+
+    const seconds = "0" + date.getSeconds();
+
+    const formattedTime = `${hours}:${minutes.substr(-2)}:${seconds.substr(-2)}`;
+
+    return formattedTime;
+  }
+
   paint(weather) {
 
     const translate = new Translate();
@@ -27,9 +58,20 @@ class UI {
         desc = data.text[0].split(', ')[1];
 
         this.location.textContent = city;
-        this.desc = desc;
-        console.log(desc)
+        this.desc.textContent = desc;
       });
- 
+    
+    this.temp.textContent = Math.floor(+(weather.main.temp) - 273) + " ℃";
+
+    this.humidity = weather.main.humidity + " %";
+    this.pressure.textContent = parseFloat(Math.floor(weather.main.pressure * 0.75)) + ' мм.рт.cт';
+
+    this.icon.setAttribute('src', `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`);
+
+    this.wind.textContent = `${weather.wind.speed} м/с, ${UI.getDirectionWind(weather.wind.deg)}`;
+    
+    this.sunrise.textContent = `Рассвет ${UI.getTime(weather.sys.sunrise)}`;
+
+    this.sunset.textContent = `Закат ${UI.getTime(weather.sys.sunset)}`;
   }
 }
